78625a45be0569003c4c16afb08880a3ea2d10f7
orenchuck <golub.ptica@gmail.com>
2020-07-25 20:06:16 +0300

    Updates

---
+++ precache-manifest.ce77df65c1638d01f1c664b2a7c904b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b8ada0ad691ed9cb7afb645131d7fd",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "5e7808a52b5457b630b5",
+    "url": "/pokemon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d6af1699cbea032b9645",
+    "url": "/pokemon/static/css/main.f0d9770f.chunk.css"
+  },
+  {
+    "revision": "5e7808a52b5457b630b5",
+    "url": "/pokemon/static/js/2.31a9c1d0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/pokemon/static/js/2.31a9c1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6af1699cbea032b9645",
+    "url": "/pokemon/static/js/main.7fdfbca0.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
